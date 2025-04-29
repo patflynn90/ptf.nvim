7d05cd26eed70068f9f0bc8079dfb4e4836ef8c8
Patrick Flynn <patflynn90@icloud.com>
2025-04-29 19:22:03 -0400

    Add LSP support

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -36,6 +36,26 @@ return {
             fzf = true,
             blink_cmp = true,
             mason = true,
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = { "italic" },
+                    hints = { "italic" },
+                    warnings = { "italic" },
+                    information = { "italic" },
+                    ok = { "italic" },
+                },
+                underlines = {
+                    errors = { "underline" },
+                    hints = { "underline" },
+                    warnings = { "underline" },
+                    information = { "underline" },
+                    ok = { "underline" },
+                },
+                inlay_hints = {
+                    background = true,
+                },
+            },
         },
         transparent_background = true,
         kitty = true,
