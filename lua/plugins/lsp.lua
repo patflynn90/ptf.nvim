a84711f65115e01d53eb751871d94b12f4555fcb
Patrick Flynn <patflynn90@icloud.com>
2025-04-29 12:54:24 -0400

    Updated keymaps to use fzf-lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,13 +33,13 @@ return {
 
         map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
         map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
---      map("grr", require("telescope.builtin").lsp_references,          "[G]oto [R]eferences")
---      map("gri", require("telescope.builtin").lsp_implementations,     "[G]oto [I]mplementation")
---      map("grd", require("telescope.builtin").lsp_definitions,         "[G]oto [D]efinition")
---      map("grD", vim.lsp.buf.declaration,                              "[G]oto [D]eclaration")
---      map("gO",  require("telescope.builtin").lsp_document_symbols,    "Open Document Symbols")
---      map("gW",  require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
---      map("grt", require("telescope.builtin").lsp_type_definitions,    "[G]oto [T]ype Definition")
+        map("grr", require("fzf-lua").lsp_references,          "[G]oto [R]eferences")
+        map("gri", require("fzf-lua").lsp_implementations,     "[G]oto [I]mplementation")
+        map("grd", require("fzf-lua").lsp_definitions,         "[G]oto [D]efinition")
+        map("grD", vim.lsp.buf.declaration,                              "[G]oto [D]eclaration")
+        map("gO",  require("fzf-lua").lsp_document_symbols,    "Open Document Symbols")
+        map("gW",  require("fzf-lua").lsp_live_workspace_symbols, "Open Workspace Symbols")
+        map("grt", require("fzf-lua").lsp_typedefs,    "[G]oto [T]ype Definition")
 
         -- Compat helper for 0.10 vs 0.11
         local function supports(client, method, bufnr)
