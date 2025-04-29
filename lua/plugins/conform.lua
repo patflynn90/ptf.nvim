5d036e5c679956c55eef6f546d68de1a9ffc3f0e
Patrick Flynn <patflynn90@icloud.com>
2025-04-29 16:14:37 -0400

    Added format on save for conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,5 +4,9 @@ return {
         formatters_by_ft = {
             lua = { "stylua" },
         },
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
     },
 }
